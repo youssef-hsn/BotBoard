cdb239f418506ba0d87433af264b804f594af627
youssef <youssefhsn16@gmail.com>
2024-11-30 08:28:49 +0200

    📋 Create Device List View

---
+++ mobile_app/lib/screens/device_list.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:botboard/widgets/device_set.dart';
+import 'package:botboard/models/devices.dart' show PairedDevice, Robot;
+
+class DeviceList extends StatelessWidget {
+  const DeviceList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var box = Hive.box('savedDevices');
+
+    List<Robot> robots = [];
+    List<PairedDevice> pairedDevices = [];
+
+    for (var device in box.values) {
+      if (device is PairedDevice) {
+        pairedDevices.add(device);
+      } else if (device is Robot) {
+        robots.add(device);
+      }
+    }
+
+    return (MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Registered Robots'),
+        ),
+        body: ListView(children: [
+          DeviceSet(
+            heading: "Robots",
+            devices: robots,
+          ),
+          DeviceSet(
+            heading: "Paired Devices",
+            devices: pairedDevices,
+          ),
+        ]),
+      ),
+    ));
+  }
+}
