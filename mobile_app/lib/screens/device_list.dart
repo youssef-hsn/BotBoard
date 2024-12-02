c607ed0a3c456a0bc3802d437675a564be1383a6
youssef <youssefhsn16@gmail.com>
2024-12-02 23:01:28 +0200

    🔥Fix issue with device_list rendering

--- mobile_app/lib/screens/device_list.dart
+++ mobile_app/lib/screens/device_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:botboard/widgets/device_set.dart';
 import 'package:botboard/models/devices.dart' show PairedDevice, Robot;
+import 'package:botboard/models/dummies.dart' as dum;
 
 class DeviceList extends StatelessWidget {
   const DeviceList({super.key});
@@ -10,14 +11,14 @@ class DeviceList extends StatelessWidget {
   Widget build(BuildContext context) {
     var box = Hive.box('savedDevices');
 
-    Set<Robot> robots = {};
-    Set<PairedDevice> pairedDevices = {};
+    Set<Robot> robots = dum.robots.toSet();
+    Set<PairedDevice> pairedDevices = dum.pairedDevices.toSet();
 
     for (var device in box.values) {
-      if (device is PairedDevice) {
-        pairedDevices.add(device);
-      } else if (device is Robot) {
+      if (device is Robot) {
         robots.add(device);
+      } else if (device is PairedDevice) {
+        pairedDevices.add(device);
       }
     }
 
