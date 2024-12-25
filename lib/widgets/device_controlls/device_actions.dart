370ca0d95633750c1496d6bf13d49af5bb8947d3
youssef <youssefhsn16@gmail.com>
2024-12-25 15:42:51 +0200

    🧠 Add device forgetting behaiviour

--- lib/widgets/device_controlls/device_actions.dart
+++ lib/widgets/device_controlls/device_actions.dart
@@ -1,8 +1,10 @@
 import 'package:botboard/models/devices.dart';
 import 'package:botboard/screens/device_details.dart';
+import 'package:botboard/widgets/alerts/confirm.dart';
 import 'package:botboard/widgets/device_controlls/main_device_action.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_classic/flutter_blue_classic.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class DeviceActions extends StatelessWidget {
   const DeviceActions({
@@ -31,7 +33,23 @@ class DeviceActions extends StatelessWidget {
                     Icons.refresh,
                     color: Theme.of(context).colorScheme.onSecondary,
                   ),
-                  onPressed: () => null,
+                  onPressed: () => {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) =>
+                          const ConfirmationAlert(
+                        consequences:
+                            "This will remove the data stored for this device. \n"
+                            "It will revert it to a paired device. with plain data",
+                      ),
+                    ).then((value) {
+                      if (value is bool && value) {
+                        Hive.box('savedDevices')
+                            .delete(widget.device.macAddress);
+                        Navigator.pop(context);
+                      }
+                    })
+                  },
                 )
               : Container(),
           MainDeviceAction(
